abd78a82cfa2637f1ac032f9419a6d576779f704
fantasyLh <m17671430910@163.com>
2024-07-24 20:53:29 +0800

    feat: 添加nuxt项目下载链接

--- lhcli/config/index.ts
+++ lhcli/config/index.ts
@@ -2,11 +2,13 @@ const admin = "后台管理系统";
 const mini = "小程序";
 const app = "app";
 const bigScreen = "数据大屏";
+const nuxt = "nuxt项目";
 
-export const projectTypeList = [admin, mini, app, bigScreen] as const;
+export const projectTypeList = [admin, mini, app, bigScreen, nuxt] as const;
 
 export const projectTypeTemplateUrl = {
   [admin]: "direct:https://gitee.com/Lh-fantasy/project-template.git#main",
+  [nuxt]: "direct:https://gitee.com/Lh-fantasy/project-template.git#nuxt",
   [mini]: "2",
   [app]: "23",
   [bigScreen]: "23",
